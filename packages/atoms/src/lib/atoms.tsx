97cf7b1aae61ac677af8530ce214b49ecb379748
Edward Coughlan <edwardcoughlan@outlook.com>
2021-09-12 13:54:11 +0100

    setup yarn work spaces

--- packages/atoms/src/lib/atoms.tsx
+++ packages/atoms/src/lib/atoms.tsx
@@ -1,4 +1,6 @@
-import './atoms.module.scss';
+import cn from 'classnames';
+import { useState } from 'react';
+import classes from './atoms.module.scss';
 
 export interface AtomsProps {
   welcomeMessage?: string;
@@ -6,10 +8,17 @@ export interface AtomsProps {
 
 const Atoms = ({
   welcomeMessage = 'Welcome to Atoms!',
-}: AtomsProps): JSX.Element => (
-  <div>
-    <h1>${welcomeMessage}</h1>
-  </div>
-);
+}: AtomsProps): JSX.Element => {
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <div
+      className={cn(classes.background, isHovered ? classes.hovered : '')}
+      onMouseEnter={(): void => setIsHovered(true)}
+      onMouseLeave={(): void => setIsHovered(false)}
+    >
+      <h1>${welcomeMessage}</h1>
+    </div>
+  );
+};
 
 export default Atoms;
