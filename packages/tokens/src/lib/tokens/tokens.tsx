5145a452d0f404b3a302b0a1d6552f11d366dddc
Edward <2625167+EdwardCoughlan@users.noreply.github.com>
2022-06-07 23:30:26 +0000

    Remove random character

--- packages/tokens/src/lib/tokens/tokens.tsx
+++ packages/tokens/src/lib/tokens/tokens.tsx
@@ -9,7 +9,6 @@ export type TokensProps = {
 export const Tokens = ({ welcomeMessage = 'Welcome to Eds Tokens!' }: TokensProps): JSX.Element => {
   const [isHovered, setIsHovered] = useState(false); return (
     <div className={cn(classes.background, isHovered ? classes.hovered : '')} onMouseEnter={(): void => setIsHovered(true)} onMouseLeave={(): void => setIsHovered(false)}>
-      {' '}
       <h1>{welcomeMessage}</h1>
     </div>
   );
