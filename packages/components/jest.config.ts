06a24ef584965672d27c74ebfa3df2c94f1d8fc8
Edward Coughlan <edwardcoughlan@outlook.com>
2022-10-17 23:48:49 +0100

    chore: [nx migration] update-jest-config-extensions

--- packages/components/jest.config.ts
+++ packages/components/jest.config.ts
@@ -1,4 +1,5 @@
 /* eslint-disable */
+/* eslint-disable */
 export default {
   displayName: 'components',
   preset: '../../jest.preset.js',
--- packages/tokens/jest.config.ts
+++ packages/tokens/jest.config.ts
@@ -1,4 +1,5 @@
 /* eslint-disable */
+/* eslint-disable */
 export default {
   displayName: 'tokens',
   preset: '../../jest.preset.js',
