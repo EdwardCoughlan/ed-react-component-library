7106bc0136ab9819b80e261137ad7f61bb7812dd
Edward Coughlan <edwardcoughlan@outlook.com>
2022-10-20 23:55:29 +0100

    update nx

--- packages/tokens/jest.config.ts
+++ packages/tokens/jest.config.ts
@@ -5,7 +5,7 @@ export default {
   displayName: 'tokens',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/tokens',
