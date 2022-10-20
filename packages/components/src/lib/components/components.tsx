046bb591cc1af41f275c76dbd0e8abbe9893bd22
Edward Coughlan <edwardcoughlan@outlook.com>
2022-10-21 00:42:35 +0100

    update nx

--- packages/components/src/lib/components/components.tsx
+++ packages/components/src/lib/components/components.tsx
@@ -3,10 +3,12 @@ import { Tokens } from '@edwardcoughlan/tokens';
 import cn from 'classnames';
 import classes from './components.module.scss';
 
-export const Components :FC = () => (
-  <div className={cn(classes.molecules)}>
-    <Tokens welcomeMessage="Hi from molecules" />
-  </div>
-);
+export const Components: FC = function () {
+  return (
+    <div className={cn(classes.molecules)}>
+      <Tokens welcomeMessage="Hi from molecules" />
+    </div>
+  );
+};
 
 export default Components;
--- packages/tokens/src/lib/tokens/tokens.tsx
+++ packages/tokens/src/lib/tokens/tokens.tsx
@@ -1,14 +1,21 @@
 import cn from 'classnames';
-import { useState } from 'react';
+import { FC, useState } from 'react';
 import classes from './tokens.module.scss';
 
 export type TokensProps = {
   welcomeMessage?: string;
 };
 
-export const Tokens = ({ welcomeMessage = 'Welcome to Eds Tokens!' }: TokensProps): JSX.Element => {
-  const [isHovered, setIsHovered] = useState(false); return (
-    <div className={cn(classes.background, isHovered ? classes.hovered : '')} onMouseEnter={(): void => setIsHovered(true)} onMouseLeave={(): void => setIsHovered(false)}>
+export const Tokens: FC<TokensProps> = ({
+  welcomeMessage = 'Welcome to Eds Tokens!',
+}: TokensProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <div
+      className={cn(classes.background, isHovered ? classes.hovered : '')}
+      onMouseEnter={(): void => setIsHovered(true)}
+      onMouseLeave={(): void => setIsHovered(false)}
+    >
       <h1>{welcomeMessage}</h1>
     </div>
   );
